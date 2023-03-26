86a4521ee4dacdb4b28267e51138450c43cc078a
naitik-lodha <naitik.lodha139@nmims.edu.in>
2023-03-26 19:50:40 +0530

    Changed overall UI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from akinator import Akinator
-from flask import render_template,Flask, request
+from flask import render_template, Flask, request, abort
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 aki = Akinator()
 
@@ -17,29 +17,23 @@ def game():
 
 @app.route('/response', methods=['POST'])
 def response():
-    # Get the user's answer
-    answer = request.form['answer']
-    if answer == 'yes':
-        aki.answer(0)
-    elif answer == 'no':
-        aki.answer( 1)
-    elif answer == 'idk':
-        aki.answer( 2)
-    elif answer == 'probably':
-        aki.answer( 3)
-    elif answer == 'probably not':
-        aki.answer( 4)
+    try:
+        # Get the user's answer
+        answer = request.form['answer']
+        aki.answer(answer)
+        # If the game is not over, ask the next question
+        if aki.progression < 80:
+            question = aki.question
+            return render_template('game.html', question=question)
+        else:
+            # The game is over, make a guess
+            aki.win()
+            guess = aki.first_guess
+            return render_template('guess.html', name=guess['name'], url=guess['absolute_picture_path'], description=guess['description'])
+    except KeyError:
+        # If the user's answer is not found in the form data, return a 400 Bad Request error
+        abort(400)
 
-    # If the game is not over, ask the next question
-    if aki.progression <= 80:
-        question = aki.question
-        return render_template('game.html', question=question)
-    else:
-        # The game is over, make a guess
-        aki.win()
-        guess = aki.first_guess
-        print(guess["name"])
-        return render_template('guess.html', name=guess['name'],url=guess['absolute_picture_path'],description=guess['description'])
 @app.route('/guesses')
 def guesses():
     li=[]
@@ -47,5 +41,16 @@ def guesses():
         if k["name"] not in li and k["name"]!=aki.first_guess["name"]:
             li.append(k["name"])
     return render_template("guesses.html",guesses=li)
-if __name__ =="__main__":
+
+# Handle 404 errors
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+# Handle 500 errors
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
     app.run(debug=True)
